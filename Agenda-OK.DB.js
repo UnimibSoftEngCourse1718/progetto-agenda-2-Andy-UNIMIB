b5641e0e7b88c6549bdd4187dc9228f7d0f9ec11
Andy@UNIMIB <a.rui1@campus.unimib.it>
2018-06-27 16:06:17 +0200

    incapsulazione database rinviata
    L'operazione richiederebbe troppo tempo.
    Per questa versione si utilizzeranno le primitive native di IndexedDB.

--- Agenda-OK.DB.js
+++ Agenda-OK.DB.js
@@ -1,42 +1,78 @@
 
-DatabaseVersion = 1;
+var	DatabaseName = 'Agenda-OK';
+var	DatabaseVersion = 2;
+var	DatabaseInitialized = false;
+var	DB = null;
+
+checkDatabaseCompatibility ();
+
+if (!DatabaseInitialized)
+
+
+
+var	request = window.indexedDB.open (DatabaseName, DatabaseVersion);
+request.onerror = function(event)
+{
+  var	msg = 'ERROR: failed opening the database ...' + event.target.errorCode;
+  alert (msg);
+  console.log (msg);
+}
+request.onsuccess = function(event)
+{
+  // Do something with request.result!
+  DB = event.target.result;
+  DatabaseInitialized = true;
+};
+request.onupgradeneeded = function (event)
+{ 
+  // Save the IDBDatabase interface 
+  var	DB = event.target.result;
+
+  console.log ('onupgradeneeded from prev version: '+event.oldVersion+' to current '+DatabaseVersion);
+  switch (event.oldVersion)
+  {
+    case 0:
+    case 1:	initialize_DB_v1 (DB); break;
+    case 2:	initialize_DB_v1 (DB); break;
+    default:	alert ('upgrade not available from version '+event.oldVersion); break;
+  }
+};
+
+
+
+
 
 // --------------------------------
-function	initDBv1 (DB)
-{
-  console.log ('initDBv1 (DB)');
-
-  // alert ('initialization of database v1 ...');
-
-  DB.createTable ('users', 'username');
-  DB.createIndex ('users', 'username', true);
-  DB.createIndex ('users', 'email',    true);
-
-  // var	events = DB.createTable ('events', 'key');
-
-  // var	toDos = DB.createTable ('todos', 'key');
-  console.log ('initDBv1 (DB) - end');
-}
-
-var	DB = new DBMS ();
-
-/*
-while (!DatabaseInitialized)
-{
-  console.log ('sleeping .2 secs ...');
-  sleep (20).then (() => { console.log ('DB initialized: resuming ...'); /* here when timeout expired * / });
-}
-*/
-
-DB.drop ('Agenda-OK');
-DB.use ('Agenda-OK', initDBv1);
-
-
 const userData = [
 		   { username: "root",  password: "admin", email: "webmaster@company.com" },
 		   { username: "pippo", password: "X",     email: "bill@company.com" },
 		   { username: "pluto", password: "Y",     email: "donna@home.org" }
 		 ];
+
+// --------------------------------
+function	initialize_DB_v1 (DB)
+{
+  console.log ('initialize_DB_v1 (DB)');
+
+  // alert ('initialization of database v1 ...');
+  var	users       = DB.createObjectStore ('users', { keyPath: 'username' });
+  var	usernameIdx = users.createIndex ('username', 'username', { unique:true });
+  var	emailIndex  = users.createIndex ('email', 'email', { unique:true });
+  users.transaction.oncomplete = function (event)
+  {
+    console.log ('adding users ...');
+    var	cursor = DB.transaction ('users', 'readwrite').objectStore ('users');
+    userData.forEach (function (user) { cursor.add (user); });
+    console.log ('users added.');
+  }
+
+  // var	events = DB.createTable ('events', 'key');
+
+  // var	toDos = DB.createTable ('todos', 'key');
+
+  console.log ('initialize_DB_v1 (DB) DONE.');
+}
+
 
 
 // DB.insert ('users', userData);
